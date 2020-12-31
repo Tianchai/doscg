8e45bf503dfb8dd221860293ad0a527b105cbba3
Tianchai Riengviwat <tianchai.riengviwat@gmail.com>
2020-12-31 21:12:23 +0700

    Install and register `fontawesome` package

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App';
 import router from './router';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
