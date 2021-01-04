51b439bab53a1f2a63b0ab05e3f2f71b629cc6e7
Tianchai Riengviwat <tianchai.riengviwat@gmail.com>
2021-01-05 05:43:52 +0700

    Register font-awesome icons

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App';
 import router from './router';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+library.add(faEnvelope, faMobileAlt, faLinkedinIn, faGithub);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
